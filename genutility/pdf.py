520b7d2ddafa7c117544a40758360830e7574b70
Dobatymo <dobatymo@users.noreply.github.com>
2023-02-01 17:30:57 +0800

    add rotate argument to pdf.join_pdfs_in_folder

--- genutility/pdf.py
+++ genutility/pdf.py
@@ -3,28 +3,42 @@ from __future__ import generator_stop
 from contextlib import ExitStack
 from os import PathLike
 from pathlib import Path
-from typing import Iterator, Union
+from typing import Iterator, Optional, Union
 
-from PyPDF2 import PdfMerger, PdfReader
+from PyPDF2 import PdfReader, PdfWriter
 
 PathType = Union[PathLike, str]
 
 
-def join_pdfs_in_folder(path_in: Path, file_out: PathType, overwrite: bool = False) -> None:
+def join_pdfs_in_folder(
+    path_in: Path,
+    file_out: PathType,
+    overwrite: bool = False,
+    rotate: Optional[int] = None,
+    password: Optional[Union[str, bytes]] = None,
+) -> None:
 
-    """Join all PDFs in `path_in` and write to `file_out`."""
-
-    merger = PdfMerger(strict=True)
+    """Join all PDFs in `path_in` and write to `file_out`.
+    `overwrite`: Overwrite existing output files
+    `rotate`: Optionally rotate pages in 90 degree increments
+    `password`: Optionally specify input file password
+    """
 
     if not path_in.exists():
         raise FileNotFoundError(path_in)
 
     with ExitStack() as stack:
         paths = sorted(path_in.glob("*.pdf"))
+        writer = PdfWriter()
+
         for path in paths:
             if path.is_file():
                 fr = stack.enter_context(path.open("rb"))
-                merger.append(fr)
+                reader = PdfReader(fr, strict=True, password=password)  # type: ignore[arg-type]
+                for page in reader.pages:
+                    if rotate is not None:
+                        page = page.rotate(rotate)
+                    writer.add_page(page)
 
         if overwrite:
             mode = "w"
@@ -32,7 +46,7 @@ def join_pdfs_in_folder(path_in: Path, file_out: PathType, overwrite: bool = Fal
             mode = "x"
 
         with open(file_out, mode + "b") as fw:
-            merger.write(fw)
+            writer.write_stream(fw)
 
 
 def iter_pdf_text(path: str) -> Iterator[str]:
@@ -74,9 +88,14 @@ def read_pdf(path: str, engine: str = "pdfminer") -> str:
 if __name__ == "__main__":
     from argparse import ArgumentParser
 
+    from genutility.args import is_dir
+
     parser = ArgumentParser(description="Merge pdf files in directory into one file.")
-    parser.add_argument("dir", help="input directory")
-    parser.add_argument("out", help="output file path")
+    parser.add_argument("dir", type=is_dir, help="input directory")
+    parser.add_argument("out", type=Path, help="output file path")
+    parser.add_argument("--rotate", type=float, help="Rotate in 90 degree increments")
+    parser.add_argument("--overwrite", action="store_true", help="Overwrite existing output files")
+    parser.add_argument("--password", type=str, help="Input file password")
     args = parser.parse_args()
 
-    join_pdfs_in_folder(args.dir, args.out)
+    join_pdfs_in_folder(args.dir, args.out, args.overwrite, args.rotate, args.password)
