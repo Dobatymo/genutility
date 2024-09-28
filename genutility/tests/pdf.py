b8000e89368172aefa417c94383d5dd09f70aaf2
Dobatymo <dobatymo@users.noreply.github.com>
2024-09-29 01:29:04 +0800

    fix pdf tests

--- genutility/tests/pdf.py
+++ genutility/tests/pdf.py
@@ -1,10 +1,17 @@
 from pathlib import Path
+from unittest import skipIf
+
+from importlib_metadata import version as get_version
+from packaging.version import Version
 
 from genutility.pdf import iter_pdf_text, join_pdfs_in_folder
 from genutility.test import MyTestCase, parametrize, print_file_diff
 
+OLD_PYPDF = Version(get_version("pypdf")) < Version("5.0.0")
+
 
 class PdfTest(MyTestCase):
+    @skipIf(OLD_PYPDF, "pypdf<5.0.0")
     @parametrize(
         ("testfiles/pdf/", "testtemp/joined.pdf", "testfiles/joined.pdf"),
     )
