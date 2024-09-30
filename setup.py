767a8dd41e22721fcf9ee5365d4fd0b75669e6ac
Dobatymo <dobatymo@users.noreply.github.com>
2024-09-30 22:05:23 +0800

    v0.0.108

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ extras_require["all"] = sorted(set(chain.from_iterable(extras_require.values()))
 setup(
     author="Dobatymo",
     name="genutility",
-    version="0.0.107",
+    version="0.0.108",
     url="https://github.com/Dobatymo/genutility",
     description="A collection of various Python utilities",
     long_description=long_description,
